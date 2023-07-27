4df9a8d039d9565f808b43e2068e9290bd852b6b
OsmanFP <osmanwako411@gmail.com>
2023-07-27 12:02:40 +0300

    create view and Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
   end
 
   def show
-    @user = User.find(params[:user_id])
-    @post = @user.posts.find(params[:id])
   end
 end
