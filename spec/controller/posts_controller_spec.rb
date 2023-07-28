775a11c4924bcaac65a39d5006ce1b8b97166718
OsmanFP <osmanwako411@gmail.com>
2023-07-28 06:01:45 +0300

    Add controller specs

---
+++ spec/controller/posts_controller_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe PostsController, type: :controller do
+  describe "Test: Index method" do
+    it "Testing for response status" do
+      get :index, params: { user_id: 1 }
+      expect(response).to have_http_status(200)
+    end
+    it "Testing for Renders the index template" do
+      get :index, params: { user_id: 1 }
+      expect(response).to render_template("index")
+    end
+  end
+  describe "Test: Show Method" do
+    it "Testing for response status" do
+      get :show, params: { user_id: 1, id: 1 }
+      expect(response).to have_http_status(200)
+    end
+    it "Testing for Renders the show template" do
+      get :show, params: { user_id: 1, id: 2 }
+      expect(response).to render_template("show")
+    end
+  end
+end
---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "Posts", type: :request do
+  describe "GET /index" do
+    describe "GET /index" do
+      pending "add some examples (or delete) #{__FILE__}"
+    end
+  end
+end
