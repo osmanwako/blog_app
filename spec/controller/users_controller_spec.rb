bd1b8fa0fd8f9a31aebc1f5cc72a9503b491a2f8
OsmanFP <osmanwako411@gmail.com>
2023-07-28 06:09:56 +0300

    Fix rubocop error

--- spec/controller/users_controller_spec.rb
+++ spec/controller/users_controller_spec.rb
@@ -1,27 +1,27 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe "Test: Index Method" do
-    it "Testing for response status" do
+  describe 'Test: Index Method' do
+    it 'Testing for response status' do
       get :index
       # //expect(response.body).to include(car_parts_path)
       expect(response).to have_http_status(:success)
     end
 
-    it "Testing for Renders the index template" do
+    it 'Testing for Renders the index template' do
       get :index
-      expect(response).to render_template("index")
+      expect(response).to render_template('index')
     end
   end
-  describe "Test: Show Method" do
-    it "Testing for response status" do
+  describe 'Test: Show Method' do
+    it 'Testing for response status' do
       get :show, params: { id: 1 }
       expect(response.status).to eq(200)
     end
 
-    it "Testing for Renders the show template" do
+    it 'Testing for Renders the show template' do
       get :show, params: { id: 1 }
-      expect(response).to render_template("show")
+      expect(response).to render_template('show')
     end
   end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,8 +1,8 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    describe "GET /index" do
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    describe 'GET /index' do
       pending "add some examples (or delete) #{__FILE__}"
     end
   end
