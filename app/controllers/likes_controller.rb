86a420dde6e1294021154a9b98e9b057590f1db2
OsmanFP <osmanwako411@gmail.com>
2023-07-29 10:48:40 +0300

    create post,comment, and like

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,12 @@
+class LikesController < ApplicationController
+  def index
+    @post = Post.find(params[:post_id])
+    @likes = Like.where(user_id: current_user.id, post_id: @post.id).first
+    unless @likes
+      @like = Like.create(user: current_user, post: @post)
+      @like.save
+      @like.update_likes_counters
+    end
+    redirect_to user_post_url(current_user, @post)
+  end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,26 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    post = post_params
+    @post = Post.create(author: current_user, title: post[:title], text: post[:text])
+    @post.save
+    @post.update_posts_counters
+    redirect_to user_post_url(@user, @post)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
