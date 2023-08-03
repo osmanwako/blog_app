99a81de40ebe19bf1abae5627c8e3633115fe850
OsmanFP <osmanwako411@gmail.com>
2023-08-03 09:11:51 +0300

    Update ruboco error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   def current_user
     @user = User.includes(:posts, :comments, :likes).last
-    if (@user.nil?)
-      @user = User.create(name: "Osman", bio: "Lecturer at JIT", photo: "")
+    if @user.nil?
+      @user = User.create(name: 'Osman', bio: 'Lecturer at JIT', photo: '')
       @user.save
-      @user = User.create(name: "Vanina", bio: "Lecturer at MIT", photo: "")
+      @user = User.create(name: 'Vanina', bio: 'Lecturer at MIT', photo: '')
       @user.save
     end
     @user
