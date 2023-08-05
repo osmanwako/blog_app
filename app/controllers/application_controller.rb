b42a949caf7fd53a2d961965fa7e436a760bc473
OsmanFP <osmanwako411@gmail.com>
2023-08-05 18:02:18 +0300

    Add API end point configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
   protected
